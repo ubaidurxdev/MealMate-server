a61864610e214232a22718ae17f48ba4a823a639
noob-ubaid <ubaidurrahman661@gmail.com>
2026-02-08 20:45:11 +0600

    add onDelete functionality in model

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application } from "express";
 const app: Application = express();
 app.get("/", (req, res) => {
-  res.send("Welcome to blog app server");
+  res.send("Welcome to MealMate server");
 });
 export default app;
