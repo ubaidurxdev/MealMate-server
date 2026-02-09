8c2ccc4a7860978f2bb08264cae11792249fba1f
noob-ubaid <ubaidurrahman661@gmail.com>
2026-02-09 09:38:02 +0600

    change the type of mealInterface

--- src/types/meal.types.ts
+++ src/types/meal.types.ts
@@ -1,12 +1,9 @@
 export interface mealInterface {
-    id : string,
-    title : string
-    description : string
-    price : number,
-    image : string,
-    isAvailable : boolean,
-    ratingAvg : number,
-    ratingCount : number,
-    createdAt : Date,
-    updatedAt : Date
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+  isAvailable: boolean;
+  ratingAvg: number;
+  ratingCount: number;
 }
