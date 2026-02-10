9c74a25e66fc5b2d38f68020a73acc8a3b9f0bac
noob-ubaid <ubaidurrahman661@gmail.com>
2026-02-10 07:32:19 +0600

    add sign up functionality

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application } from "express";
 import { mealsRouter } from "./modules/meals/meals.router";
 import errorHandler from "./middlewares/globalErrorHandler";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 const app: Application = express();
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to MealMate server");
 });
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use("/api/provider", mealsRouter);
 app.use(errorHandler)
 export default app;
